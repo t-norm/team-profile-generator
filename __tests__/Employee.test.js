4af772fbf924316639abdfb276e1b40564656198
Tyler Norman <tnorman600@gmail.com>
2021-11-10 11:38:29 -0700

    red, green, refactor for employee test.

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,28 @@
+const Employee = require('../lib/Employee');
+
+test('create new employee object', () => {
+    const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');
+    expect(employee.name).toBe('John Doe');
+    expect(employee.ID).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+})
+
+test('gets employees name', () => {
+    const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');
+    expect(employee.getName()).toHaveProperty('name');
+})
+
+test('gets employees ID number', () => {
+    const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');   
+    expect(employee.getID()).toHaveProperty('ID');
+})
+
+test('get the employee email', () => {
+    const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');  
+    expect(employee.getEmail()).toHaveProperty('email');
+})
+
+test('get the employee role', () => {
+    const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');
+    expect(employee.getRole()).toBe('Employee');
+})
