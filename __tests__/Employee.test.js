877aedc192bba2f8e4859585f7702a5b8d757cee
Tyler Norman <tnorman600@gmail.com>
2021-11-10 12:11:28 -0700

    minor syntax/housekeeping changes to test suites

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,24 +5,24 @@ test('create new employee object', () => {
     expect(employee.name).toBe('John Doe');
     expect(employee.ID).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-})
+});
 
 test('gets employees name', () => {
     const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');
     expect(employee.getName()).toHaveProperty('name');
-})
+});
 
 test('gets employees ID number', () => {
     const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');   
     expect(employee.getID()).toHaveProperty('ID');
-})
+});
 
 test('get the employee email', () => {
     const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');  
     expect(employee.getEmail()).toHaveProperty('email');
-})
+});
 
 test('get the employee role', () => {
     const employee = new Employee('John Doe', 1, 'heresjohhny@shining.com');
     expect(employee.getRole()).toBe('Employee');
-})
+});
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -6,14 +6,14 @@ test('creates new engineer object', () => {
     expect(engineer.ID).toEqual(expect.any(Number));
     expect(engineer.email).toEqual(expect.any(String));
     expect(engineer.githubUser).toEqual(expect.any(String));
-})
+});
 
 test('gets engineer role', () => {
     const engineer = new Engineer('John Doe', 1, 'heresjohnny@shining.com', 'githubUser');
-    expect(engineer.getRole()).toBe('Engineer')
-})
+    expect(engineer.getRole()).toBe('Engineer');
+});
 
 test('gets engineer github', () => {
     const engineer = new Engineer('John Doe', 1, 'heresjohnny@shining.com', 'githubUser');
-    expect(engineer.getGithubUser()).toBe(`https://github.com/${engineer.githubUser}`)
-})
+    expect(engineer.getGithubUser()).toBe(`https://github.com/${engineer.githubUser}`);
+});
